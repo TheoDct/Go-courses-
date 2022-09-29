bb44f6b21c2e4f552134d710d2c1f6f080b272d5
TheoDct <theo.diancourt@algosup.com>
2022-09-29 10:44:33 +0200

    test is passing

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", PlayerServer)
 	http.ListenAndServe(":8080", nil)
+	fmt.Println("starting the server on port 8080")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+	fmt.Fprint(w, "hello", r.URL.Path)
 }
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
