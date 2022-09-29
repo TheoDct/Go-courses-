33619b79c7ea05b4a0b5a88e5f336f2864a08859
TheoDct <theo.diancourt@algosup.com>
2022-09-29 11:44:56 +0200

    getplayers done

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", PlayerServer)
+	http.HandleFunc("/players/", PlayerServer)
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("starting the server on port 8080")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello", r.URL.Path)
-}
-
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	name := strings.Split(r.URL.Path, "/")[2]
+	score := GETPlayers(name)
+	fmt.Fprint(w, score)
 }
 
-func GETPlayers() string {
-	return "20"
+func GETPlayers(name string) int {
+	if name == "Pepper" {
+		return 20
+	} else {
+		return 10
+	}
 }
--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestGETPlayers(t *testing.T) {
+func TestGETPepper(t *testing.T) {
 	request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
 	response := httptest.NewRecorder()
 
@@ -17,5 +17,35 @@ func TestGETPlayers(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestGETSalt(t *testing.T) {
+	request, _ := http.NewRequest(http.MethodGet, "/players/Salt", nil)
+	response := httptest.NewRecorder()
+
+	PlayerServer(response, request)
+
+	got := response.Body.String()
+	want := "10"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+func TestPlayers(t *testing.T) {
+
+	gotPepper := GETPlayers("Pepper")
+	wantPepper := 20
+
+	gotSalt := GETPlayers("Salt")
+	wantSalt := 10
+	if gotPepper != wantPepper {
+		t.Errorf("got %q, want %q", gotPepper, wantPepper)
+
+	}
+
+	if gotSalt != wantSalt {
+		t.Errorf("got %q, want %q", gotSalt, wantSalt)
 	}
 }
